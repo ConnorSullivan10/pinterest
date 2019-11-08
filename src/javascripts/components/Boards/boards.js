09b4cfbe05ed6d83f08ac928593e181ca95eb7e0
Connor Sullivan <cksullvn@gmail.com>
2019-11-07 22:46:03 -0600

    singleBoard printing and event listeners are working, just need to figure out how to get the pins to print right

--- src/javascripts/components/Boards/boards.js
+++ src/javascripts/components/Boards/boards.js
@@ -1,49 +1,61 @@
+import firebase from 'firebase';
+import $ from 'jquery';
 import './boards.scss';
 import utilities from '../../helpers/utilities';
 import boardData from '../../helpers/data/boardData';
 import boardCard from '../BoardCard/boardCard';
-import pin from '../Pins/pins';
+import pinData from '../../helpers/data/pinData';
 
-// PRINTS MAIN BOARDS
+// PRINTS MAIN BOARDS & EVENT LISTENER FOR BIG CARD
 const boardsComponent = (uid) => {
   boardData.getBoardByUid(uid)
     .then((boards) => {
       let domString = '';
       domString += '<h1>BOARDS</h1>';
-      domString += '<div id="big-board-view" class="d-flex flex-wrap"></div>';
       domString += '<div id="board-container" class="d-flex flex-wrap">';
       boards.forEach((board) => {
         domString += boardCard.createBoard(board);
       });
       domString += '</div>';
       utilities.printToDom('boards', domString);
+      // eslint-disable-next-line no-use-before-define
+      $('#boards').on('click', '.boardCard', printBigBoard);
     });
 };
 
-// Attempt to print boards and single big card div on click
+// PRINTS BIG CARD AND EVENT LISTENER FOR CLOSE BUTTON
 
-const clickToPrintBigBoard = () => {
-  $('.boardCard').on('click', (event) => {
-    const singleBoard = event.target.id;
-    let bigBoardString = '';
-    boards.forEach((board) => {
-      const boardName = board.name.toLowerCase();
-      if (singleBoard === `${board.id}`) {
+const boardsToHide = $('#boards');
+
+const printBigBoard = (event) => {
+  const { uid } = firebase.auth().currentUser;
+  const singleBoard = event.target.id;
+  let bigBoardString = `<div class="big-board-title card text-center" id="${singleBoard}">
+    <h2>$(singleBoard)</h2><button class="close d-flex justify-content-end" style="color:black;">X</button>
+    </div>`;
+  bigBoardString += '<div id="pinned-cards" class="d-flex flex-wrap">';
+  pinData.getPinByBoardId(singleBoard)
+    .then((pins) => {
+      boardsToHide.empty();
+      pins.forEach((pin) => {
         bigBoardString += `
-      <div class="big-board-card Card text-center ${boardName}" id="bigBoard-${board.id}">
-         <button class="close d-flex justify-content-end" style="color:red;">X</button>
-         <h2>${board.name}</h2>
-         <h3>${board.boardDescription}</h3>
-         <div id="pinned-cards" class="d-flex flex-wrap"></div>'
-     </div>`;
-        utilities.printToDom('big-board-view', bigBoardString);
-        pin.makeAPin(board.id);
-      }
-    });
-    utilities.printToDom('board-container', '');
-  });
+        <div class="card col-4 pinCard" id="${pin.id}">
+          <img src=${pin.imageURL} class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title" id="pin>${pin.name}</h5>
+            <p>${pin.description}
+          </div>
+        </div>`;
+      });
+      bigBoardString += '</div>';
+      utilities.printToDom('big-board-view', bigBoardString);
+      $('#big-board-view').on('click', '.close', () => {
+        $('#big-board-view').empty();
+        boardsComponent(uid);
+      });
+    })
+    .catch((error) => console.error(error));
 };
-
 
 // // PRINTS MAIN BOARDS AND BIG BOARDS (hidden)
 // const boardsComponent = (uid) => {
@@ -71,4 +83,4 @@ const clickToPrintBigBoard = () => {
 //     });
 // };
 
-export default { boardsComponent, clickToPrintBigBoard };
+export default { boardsComponent };
--- src/javascripts/components/Pins/pins.js
+++ src/javascripts/components/Pins/pins.js
@@ -1,49 +1,49 @@
-import './pins.scss';
-import utilities from '../../helpers/utilities';
-import pinData from '../../helpers/data/pinData';
+// import './pins.scss';
+// import utilities from '../../helpers/utilities';
+// import pinData from '../../helpers/data/pinData';
 
 
-const makeAPin = (boardID) => {
-  pinData.getPinByBoardId(boardID)
-    .then((pins) => {
-      let domString = '';
-      pins.forEach((pin) => {
-        if (boardID === `${pin.boardID}`) {
-          domString += `
-        <div class="card col-4 pinCard" id="${pin.id}>
-          <img src=${pin.imageURL} class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">${pin.name}</h5>
-            <p>${pin.description}
-          </div>
-        </div>`;
-        } else {
-          domString += '';
-        }
-      });
-      utilities.printToDom('pinned-cards', domString);
-    });
-};
-
-// ALTERNATE VERSION OF FUNCTION ABOVE
 // const makeAPin = (boardID) => {
-//   const pins = pinData.getPinByBoardId(boardID);
-//   let domString = '';
-//   pins.forEach((pin) => {
-//     if (boardID === pin.boardID) {
-//       domString += `
-//     <div class="card col-4 pinCard" id="${pin.id}>
-//       <img src=${pin.imageURL} class="card-img-top" alt="...">
-//       <div class="card-body">
-//         <h5 class="card-title">${pin.name}</h5>
-//         <p>${pin.description}
-//       </div>
-//     </div>`;
-//     } else {
-//       domString += '';
-//     }
-//   });
-//   utilities.printToDom('pinned-cards', domString);
+//   pinData.getPinByBoardId(boardID)
+//     .then((pins) => {
+//       let domString = '';
+//       pins.forEach((pin) => {
+//         if (boardID === `${pin.boardID}`) {
+//           domString += `
+//         <div class="card col-4 pinCard" id="${pin.id}>
+//           <img src=${pin.imageURL} class="card-img-top" alt="...">
+//           <div class="card-body">
+//             <h5 class="card-title">${pin.name}</h5>
+//             <p>${pin.description}
+//           </div>
+//         </div>`;
+//         } else {
+//           domString += '';
+//         }
+//       });
+//       utilities.printToDom('pinned-cards', domString);
+//     });
 // };
 
-export default { makeAPin };
+// // ALTERNATE VERSION OF FUNCTION ABOVE
+// // const makeAPin = (boardID) => {
+// //   const pins = pinData.getPinByBoardId(boardID);
+// //   let domString = '';
+// //   pins.forEach((pin) => {
+// //     if (boardID === pin.boardID) {
+// //       domString += `
+// //     <div class="card col-4 pinCard" id="${pin.id}>
+// //       <img src=${pin.imageURL} class="card-img-top" alt="...">
+// //       <div class="card-body">
+// //         <h5 class="card-title">${pin.name}</h5>
+// //         <p>${pin.description}
+// //       </div>
+// //     </div>`;
+// //     } else {
+// //       domString += '';
+// //     }
+// //   });
+// //   utilities.printToDom('pinned-cards', domString);
+// // };
+
+// export default { makeAPin };
