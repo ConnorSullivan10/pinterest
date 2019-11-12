565ce8fc8025887bad76e970c77bc5578bcfa45d
Connor Sullivan <cksullvn@gmail.com>
2019-11-11 19:44:22 -0600

    got addBoards function to work

--- src/javascripts/components/Boards/boards.js
+++ src/javascripts/components/Boards/boards.js
@@ -9,30 +9,26 @@ import pinData from '../../helpers/data/pinData';
 import './boards.scss';
 
 
-// // FUNCTION TO ADD NEW BOARD TO ARRAY FROM BOARDS.JSON
+// FUNCTION TO ADD NEW BOARD TO ARRAY FROM BOARDS.JSON
 
-// let counter = 4;
-
-// const addNewBoard = (e) => {
-//   e.stopImmediatePropagation();
-//   const { uid } = firebase.auth().currentUser;
-//   const newBoard = {
-//     id: `board${counter}`,
-//     imageURL: $('#board-image-url').val(),
-//     name: $('#board-name').val(),
-//     uid,
-//     isPrivate: true,
-//     boardDescription: $('#board-description').val(),
-//   };
-//   counter += 1;
-//   boardData.addNewBoard(newBoard)
-//     .then(() => {
-//       $('#exampleModal').modal('hide');
-//       // eslint-disable-next-line no-use-before-define
-//       boardsComponent(uid);
-//     })
-//     .catch((error) => console.error(error));
-// };
+const addNewBoard = (e) => {
+  e.stopImmediatePropagation();
+  const { uid } = firebase.auth().currentUser;
+  const newBoard = {
+    imageURL: $('#board-image-url').val(),
+    name: $('#board-name').val(),
+    uid,
+    isPrivate: true,
+    boardDescription: $('#board-description').val(),
+  };
+  boardData.addNewBoard(newBoard)
+    .then(() => {
+      $('#exampleModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      boardsComponent(uid);
+    })
+    .catch((error) => console.error(error));
+};
 
 // // FUNCTION TO ADD NEW PIN TO ARRAY FROM PINS.JSON
 
@@ -113,7 +109,7 @@ const boardsComponent = (uid) => {
       // eslint-disable-next-line no-use-before-define
       $('#boards').on('click', '.delete-board', deleteBoardAndPins);
       // eslint-disable-next-line no-use-before-define
-      // $('#boards').on('click', '#add-new-board', addNewBoard);
+      $(document.body).on('click', '#add-new-board', addNewBoard);
       // eslint-disable-next-line no-use-before-define
       $('#big-board-view').on('click', '.delete-pin', deletePinFromBoard);
       // $('#boards').on('click', '#add-new-pin', addNewPin);
