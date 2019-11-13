221058e2bb9720677547d5db169ef6c547b327a5
Connor Sullivan <cksullvn@gmail.com>
2019-11-13 15:19:49 -0600

    board options are now showing in the dropdown at the bottom of each pin, but reassignment is not fully functional yet

--- src/javascripts/components/Pins/pins.js
+++ src/javascripts/components/Pins/pins.js
@@ -10,12 +10,11 @@ const boardsToHide = $('#boards');
 
 const buildDropdownSelection = () => new Promise((resolve, reject) => {
   const { uid } = firebase.auth().currentUser;
+  let bigBoardString = '';
   boardData.getBoardByUid(uid)
     .then((boards) => {
-      let bigBoardString = '';
       boards.forEach((board) => {
         bigBoardString += `<a class="dropdown-item" href="#" data-boardID="${board.id}">${board.name}</a>`;
-        console.log(bigBoardString);
       });
       resolve(bigBoardString);
     })
@@ -23,40 +22,41 @@ const buildDropdownSelection = () => new Promise((resolve, reject) => {
 });
 
 const createPinsOnBoard = (singleBoard) => {
+  let boardSelections = '';
+  buildDropdownSelection()
+    .then((resolve) => {
+      boardSelections = resolve;
+    });
   let bigBoardString = `
-    <div class="big-board-title card text-center" id="${singleBoard}">
-        <h2>${singleBoard}</h2>
-        <button class="close d-flex justify-content-end" style="color:black;">X</button>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#examplePinModal">
-          Add Pin
-        </button>
-    </div>`;
+  <div class="big-board-title card text-center" id="${singleBoard}">
+  <h2>${singleBoard}</h2>
+  <button class="close d-flex justify-content-end" style="color:black;">X</button>
+  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#examplePinModal">
+  Add Pin
+  </button>
+  </div>`;
   bigBoardString += '<div id="pinned-cards" class="d-flex flex-wrap">';
   pinData.getPinByBoardId(singleBoard)
     .then((pins) => {
       boardsToHide.empty();
       pins.forEach((pin) => {
+        console.log(boardSelections);
         bigBoardString += `
-          <div class="card col-4 pinCard card-body"  id="${pin.id}">
-            <img src="${pin.imageURL}" class="card-img-top" alt="..."/>
-            <h5 class="card-title" id="pin">${pin.name}</h5>
-            <p>${pin.description}</p>
-            <a href="#${pin.siteURL}" class="btn btn-info" role="button">Link</a>
-            <button class="btn btn-danger delete-pin" data-boardID="${pin.boardID}" id="${pin.id}">Remove Pin</button>
-            <div class="dropdown">
-              <button class="btn btn-secondary dropdown-toggle" data-boardID="${pin.boardID} type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Change Board
-              </button>
-              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" data-boardID="${pin.boardID} id="dropdownMenuButton-${pin.id}">
-              `;
-        buildDropdownSelection()
-          .then((resolve) => {
-            bigBoardString += resolve;
-            bigBoardString += '</div></div></div>';
-          });
+        <div class="card col-4 pinCard card-body"  id="${pin.id}">
+        <img src="${pin.imageURL}" class="card-img-top" alt="..."/>
+        <h5 class="card-title" id="pin">${pin.name}</h5>
+        <p>${pin.description}</p>
+        <a href="#${pin.siteURL}" class="btn btn-info" role="button">Link</a>
+        <button class="btn btn-danger delete-pin" data-boardID="${pin.boardID}" id="${pin.id}">Remove Pin</button>
+        <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" data-boardID="${pin.boardID} type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Change Board
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" data-boardID="${pin.boardID} id="dropdownMenuButton-${pin.id}">
+        ${boardSelections}</div></div></div>
+        `;
       });
       bigBoardString += '</div>';
-      console.log('helloworld', bigBoardString);
       utilities.printToDom('big-board-view', bigBoardString);
     })
     .catch((error) => console.error(error));
